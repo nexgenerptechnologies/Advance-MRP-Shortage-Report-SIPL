52c52028ae48b8015d9a213c90ee883491c2585b
Regis Digital <regissdigital@gmail.com>
2026-06-03 07:53:47 +0530

    Remove broken patch to unblock deployment

--- mrp_shortage_report/patches/v1_0/fix_report_type.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-def execute():
-    if frappe.db.exists('Report', 'MRP Shortage Report'):
-        frappe.db.set_value('Report', 'MRP Shortage Report', 'report_type', 'Script Report')
-        frappe.db.set_value('Report', 'MRP Shortage Report', 'query', '')
-        frappe.db.commit()
