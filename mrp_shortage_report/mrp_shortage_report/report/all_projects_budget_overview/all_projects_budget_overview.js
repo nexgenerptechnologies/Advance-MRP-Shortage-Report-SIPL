ca5bd725441a0eb9566f2cfc487e852efaf0f4a4
Regis Digital <regissdigital@gmail.com>
2026-07-12 14:26:55 +0530

    Fix JS undefined error in formatter that broke datatable rendering

--- mrp_shortage_report/mrp_shortage_report/report/all_projects_budget_overview/all_projects_budget_overview.js
+++ mrp_shortage_report/mrp_shortage_report/report/all_projects_budget_overview/all_projects_budget_overview.js
@@ -4,11 +4,11 @@ frappe.query_reports["All Projects Budget Overview"] = {
 		value = default_formatter(value, row, column, data);
 		
 		if (column.fieldname == "percent_expended" || column.fieldname == "percent_committed") {
-			if (data[column.fieldname] > 100) {
+			if (data && data[column.fieldname] > 100) {
 				value = "<span style='color:red'>" + value + "</span>";
-			} else if (data[column.fieldname] == 100) {
+			} else if (data && data[column.fieldname] == 100) {
 				value = "<span style='color:orange'>" + value + "</span>";
-			} else {
+			} else if (data && data[column.fieldname] != null) {
 				value = "<span style='color:green'>" + value + "</span>";
 			}
 		}
